887015626b31da3813d515085c5462495659536c
Henrik Treadup <henrik.treadup@gmail.com>
2019-07-21 08:56:25 +0200

    Trivial https web server

---
+++ handling_requests/trivialtlsserver.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: nil,
+	}
+
+	server.ListenAndServeTLS("cert.pem", "key.pem")
+}
